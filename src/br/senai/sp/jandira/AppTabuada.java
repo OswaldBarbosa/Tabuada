e7d011c5305b3b89ce87c6bf83eeb79331f28c26
OSWALDO DA SILVA BARBOSA DOS SANTOS <22282212@CFP127ALUNO.LOCAL>
2022-09-06 16:59:46 -0300

    classe tabuada completa

--- src/br/senai/sp/jandira/AppTabuada.java
+++ src/br/senai/sp/jandira/AppTabuada.java
@@ -1,5 +1,27 @@
 package br.senai.sp.jandira;
 
-public class AppTabuada {
+import java.util.Scanner;
 
+import br.senai.sp.jandira.model.Tabuada;
+
+public class AppTabuada {
+	
+	public static void main(String[] args) {
+	
+	Scanner leitor = new Scanner(System.in);
+	
+	Tabuada Oswaldo = new Tabuada();
+
+	System.out.println("multiplicando: ");
+	leitor.nextInt();
+	
+	System.out.println("minimoMultiplicador");
+	leitor.nextInt();
+	
+	System.out.println("maximoMultiplicador");
+	leitor.nextInt();
+	
+	System.out.println(Oswaldo.getresultado());
+	
+	}
 }
---
+++ src/br/senai/sp/jandira/model/Tabuada.java
@@ -0,0 +1,28 @@
+package br.senai.sp.jandira.model;
+
+public class Tabuada {
+	
+	public int multiplicando;
+	public int minimoMultiplicador;
+	public int maximoMultiplicador;
+	
+	public String[] getTabuada() {
+		
+		int tamanho = maximoMultiplicador - minimoMultiplicador + 1;
+		String tabuada[] = new String[tamanho];	
+		int i = 0;
+		
+		while (minimoMultiplicador <= maximoMultiplicador) {
+			
+			int produto = multiplicando * minimoMultiplicador;
+			
+			String resultado = multiplicando + " X " + minimoMultiplicador + " = " + produto;
+			tabuada[i] = resultado;
+			
+			i++;
+			multiplicando++;
+		}
+		
+		return tabuada;
+	}
+}
